d206df0e2d791770937c86f38d8ca0032bd0c092
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-04 12:02:02 -0700

    Initial Sync

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.quantity').on('keyup', function() {
+		var quantity = +$(this).val();
+		var price = +$(this).closest('.list-items').data('price');
+		$(this).find('.total').text(price * quantity);
+		$('.cart-total').text(+'.total');
+	});
+});
